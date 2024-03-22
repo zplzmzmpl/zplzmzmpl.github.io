8d62da5dfa5e8dcfb4e75706b0408f0e0a1eb4de
zplzmzmpl <hhgghhv532@gmail.com>
2024-03-22 20:20:36 +0800

    fisrt commit

---
+++ projects.js
@@ -0,0 +1,50 @@
+var projects = [{
+        title: '',
+        descr: '',
+        img: 'images/02.jpg',
+        url: 'https://github.com/zplzmzmpl/Regional-ecological-time-series-monitoring-based-on-LandTrendr'
+    },
+    {
+      title: '',
+      descr: '',
+      img: 'images/01.jpg',
+      url: 'https://github.com/zplzmzmpl/Secondary_development_based_on_ArcEngine'
+    },
+    {
+      title: '',
+      descr: '',
+      img: 'images/04.jpg',
+      url: 'https://github.com/zplzmzmpl/remote_sensing_images_deep_learning_using__RS32_dataset'
+    },
+    {
+      title: '',
+      descr: '',
+      img: 'images/05.jpg',
+      url: 'https://github.com/zplzmzmpl/fisher-bayes_classifier_based_on_images'
+    },
+    {
+      title: '',
+      descr: '',
+      img: 'images/06.jpg',
+      url: 'https://github.com/zplzmzmpl/Dongting-Lake-Wetland-Multi-source-Remote-Sensing-Monitoring-System'
+    },
+    {
+      title: '',
+      descr: '',
+      img: 'images/07.jpg',
+      url: 'https://github.com/zplzmzmpl/object_detection'
+    },
+
+    /*{
+    	title: '',
+    	descr: 'A visual demonstration of a satellite image cloud and cloud shadow identification algorithm I developed. It uses CSS to change the position of an image sprite on hover. This method is much quicker than loading a replacement image, which allows for a seamless transition - a better user experience.',
+    	img: 'images/msscvm_examples.jpg',
+    	url: 'https://jdbcode.github.io/MSScvm/examples.html'
+    },*/
+    /*{
+    	title: '',
+    	descr: 'A description page (under construction) for a Landsat-based change detection algorithm. This page uses a scroll position tracking JS library to animate the entry and exit of elements.',
+    	img: 'images/landtrendr_descript.jpg',
+    	url: 'http://emapr.ceoas.oregonstate.edu/pages/tools/landtrendr/landtrendr.html'
+    }*/
+]
